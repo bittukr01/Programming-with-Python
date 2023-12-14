39c7226e75723ef6cee6e7c0fc08218a0862260c
Bittukumar0610 <bittu.c.2424@iibm.in>
2023-12-15 00:33:28 +0530

    string examples

---
+++ string.py
@@ -0,0 +1,15 @@
+#print("Hello World")
+
+#method1:
+#print('Python's World')  #Invalid Syntax
+
+print("Python's World")
+
+#method2:
+print('Python\'s World')
+
+#method3:
+print("Python's" ' World')
+
+#method4:
+print('Python\'s "World"')
