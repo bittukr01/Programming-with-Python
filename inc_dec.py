b655198b28c9c7419f276401804ddb26689b713a
Bittukumar0610 <bittu.c.2424@iibm.in>
2023-12-15 01:18:12 +0530

    Example increment and decrement

---
+++ inc_dec.py
@@ -0,0 +1,35 @@
+#Increment and Decrement:
+
+a=5
+a=a+1
+print(a)
+
+#Or
+
+b=6
+b+=1
+print(b)
+
+#Or
+
+p="Python"
+p+=" World"
+print(p)
+
+#Decrement
+
+c=10
+c-=5
+print(c)
+
+#Multiplication
+
+d="Bittu"
+d*=3
+print(d)
+
+#Div
+z=6
+z//=2
+print(z)
+
