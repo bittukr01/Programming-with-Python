911ceb485b350285b03cbe1786d92739070df122
Bittukumar0610 <bittu.c.2424@iibm.in>
2023-12-16 00:36:08 +0530

    some practices

---
+++ challenge.py
@@ -0,0 +1,27 @@
+'''
+#Celsius and Farenheit
+
+c=input("Celsius: ")
+c=float(c)
+f=(c * 9/5) + 32
+print("Farenheit: ",f)
+
+
+#Farenheit and Celsius
+
+f=input("Farenheit: ")
+f=float(f)
+c=(f - 32) * 5/9
+print("Celsius: ",c)
+
+'''
+
+#Simple Interest=(p*r*t)/100
+
+p=int(input("Enter Principle: "))
+r=int(input("Enter Rate: "))
+t=int(input("Enter Time: "))
+si=(p*r*t)/100
+print(si)
+
+
